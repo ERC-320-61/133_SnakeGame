d9a67ba26f871315a9f4dfb52db32929021ef576
Eric <95461143+ERC-320-61@users.noreply.github.com>
2024-03-27 17:58:22 -0700

    Delete app/src/main/java/com/gamecodeschool/snake/mechanics directory
    not needed

--- app/src/main/java/com/gamecodeschool/snake/mechanics/TouchHandler.java
+++
@@ -1,7 +0,0 @@
-package com.gamecodeschool.snake.mechanics;
-
-import android.view.MotionEvent;
-
-public abstract class TouchHandler {
-    public abstract boolean handleTouchEvent(MotionEvent event);
-}
